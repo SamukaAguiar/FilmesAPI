8320c7d4670719227b1628f49b5632c0c121bc17
Samuel Peres de Aguiar <samuel@linqcode.com.br>
2022-11-29 18:14:29 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;  
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecurperarFilmePorId), new { Id = filme.Id}, filme);
+
+        }
+
+        [HttpGet]
+        public IActionResult RecurperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecurperarFilmePorId(int id) 
+        {
+            Filme filme = filmes.First(filme => filme.Id == id);
+            if (filme != null)
+                return Ok(filme);
+            else
+                return NotFound();
+        }
+    }
+
+    
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required]        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Campo titulo é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero não pode ter mais que 30 caracter")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duracao deve ter no minimo 1 e no maximo 600")]
+        public int Duracao { get; set; }
+
+    }
+}
